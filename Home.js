d245662eeec199fc1a27ab8f616fb9138bcee5c7
YOUR <christophericp10@gmail.com>
2026-06-25 10:57:17 -0400

    q

--- Home.js
+++ Home.js
@@ -1,4 +1,24 @@
 let home = document.querySelector('home');
 let about = document.querySelector('about');
-let portfolio = document.querySelector('portfolio');
+let portButton = document.querySelector('#portfolio');
 
+let i = 0;
+let txt = 'Christopher Castellon-Perez';
+let speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("name").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = function() {
+    typeWriter();
+};
+
+
+portButton.addEventListener('click', function() {
+  window.location.href = 'http://127.0.0.1:3000/Projects.html?vscode-livepreview=true';
+})
